df3d7b26d54aa99f5a5c748876741cb851a4438b
ahmedusama369 <96350734+ahmedusama369@users.noreply.github.com>
2021-12-19 20:18:34 +0200

    Update solution.cpp

--- Task1/problem 3/solution.cpp
+++ Task1/problem 3/solution.cpp
@@ -1,34 +1,32 @@
 #include<iostream>
 using namespace std;
-double BinarySearch(int* arr, int n, int no, int begin, int end);
+int BinarySearch(int* arr, int n, int l, int r);
 int main()
 {
-    int arr[] = { 1,2,4,5,8,9 };
-    int x = BinarySearch(arr, 6, 10, 0, 6);
-    cout << x;
-
+    int arr[7] = { 1,2,3,5,8,10,13 };
+    int y = BinarySearch(arr, 50, 0, 6);
+    cout << y;
     return 0;
 }
-double BinarySearch(int* arr, int n, int no, int begin, int end)
+int BinarySearch(int* arr, int n, int l, int r)
 {
-    int i;
-    i = (end + begin) / 2;
-    if (arr[i] == no)
+    int mid = (r + l) / 2;
+    if (r != l || (arr[l] == n && arr[r] == n))
     {
-        return i;
-    }
-    else
-    {
-        if (arr[i] > no)
+        if (arr[mid] == n) return mid;
+        else if (arr[mid] > n)
         {
-            end = i - 1;
-            return BinarySearch(arr, n, no, begin, end);
+            r = mid - 1;
+            return BinarySearch(arr, n, l, r);
+
         }
-        else if (arr[i] < no)
+        else if (arr[mid] < n)
         {
-            begin = i + 1;
-            return BinarySearch(arr, n, no, begin, end);
+            l = mid + 1;
+            return BinarySearch(arr, n, l, r);
+
         }
     }
     return -1;
+
 }
