7305bdc213e55f1eae415381eafd93a857c36e85
ahmedusama369 <96350734+ahmedusama369@users.noreply.github.com>
2021-12-20 19:36:00 +0200

    Update solution.cpp

--- Task1/problem2/solution.cpp
+++ Task1/problem2/solution.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
-	for (int i = 2; i < n; i++)
-	{
-		if (i == 2 || i == 3 || i==5 || i==7) cout << i << " ";
-		else if (i % 2 != 0 && i % 3 != 0 && i%5!=0 && i%7!=0) cout << i << " ";
-	}
-	return 0;
+    int n;
+    cin>>n;
+    cout<<2<<" "<<3<<" ";
+    for(int i=4;i<n;i++)
+    {
+        int x=0;
+        for(int j=2;j<=(i/2)+1;j++)
+        {
+            if(i%j==0) {
+                x++;
+                break;
+            }
+
+        }
+        if(x==0) cout<<i<<" ";
+
+    }
+    return 0;
 }
