071e45873e29cc922244437a8f6bca79cc5cbf70
ahmedusama369 <96350734+ahmedusama369@users.noreply.github.com>
2021-12-20 19:48:34 +0200

    Update solution.cpp

--- Task1/problem5/solution.cpp
+++ Task1/problem5/solution.cpp
@@ -1,21 +1,17 @@
 #include<iostream>
 using namespace std;
 int sum(int* arr, int n);
+int sumf(int* arr,int n);
+int sumw(int* arr,int n);
 int main()
 {
     int n;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++) cin >> arr[i];
-    int sum1 = 0;
-    for (int i = 0; i < n; i++) sum1 = sum1 + arr[i];
-    int sum2 = 0;
-    int j = 0;
-    while (j < n)
-    {
-        sum1 = sum1 + arr[j];
-        j++;
-    }
+    int r1=sum(arr,n);
+    int r2=sumf(arr,n);
+    int r3=sumw(arr,n);
 }
 
 int sum(int* arr, int n)
@@ -23,5 +19,21 @@ int sum(int* arr, int n)
     if (n == 0) return 0;
     return arr[n - 1] + sum(arr, n - 1);
 }
+int sumf(int* arr,int n)
+{
+    int sum1 = 0;
+    for (int i = 0; i < n; i++) sum1 = sum1 + arr[i];
+    return sum1;
+}
 
-
+int sumw(int* arr,int n)
+{
+    int sum2 = 0;
+    int j = 0;
+    while (j < n)
+    {
+        sum2 = sum2 + arr[j];
+        j++;
+    }
+    return sum2;
+}
