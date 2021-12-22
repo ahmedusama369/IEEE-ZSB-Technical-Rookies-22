410f035a015464b23e6243cda41948e7cdbc5887
ahmedusama369 <96350734+ahmedusama369@users.noreply.github.com>
2021-12-23 00:58:13 +0200

    Update solution.py

--- Task2/problem3/solution.py
+++ Task2/problem3/solution.py
@@ -1,7 +1,6 @@
 import random
-n=random.randint(100, 999)
+n=random.randint(100,999)
 n=str(n)
-print(n)
 j=0
 while(True):
     c=input()
@@ -11,12 +10,10 @@ while(True):
     hit=0
     misses=0
     for i in range(len(n)):
-        for j in range(len(n)):
-            if n[i]==c[j] and i==j:
-                hit=hit+1
-                break
-            elif n[i]==c[j] and i!=j:
-                misses=misses+1
-                break
+        if c[i]==n[i]:
+            hit=hit+1
+        elif c[i] in n and c[i]!=n[i]:
+            misses=misses+1
+   
     print(hit,"hit",misses,"misses")
 print(j)
